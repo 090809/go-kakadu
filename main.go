104f348ac699ec92648d60dffc2845ed16860b7b
Torquemada <darkmasters@yandex.ru>
2019-06-20 16:37:22 +0300

    Adding .env files to prj

--- main.go
+++ main.go
@@ -8,13 +8,20 @@ import (
 	//"fmt"
 	// "net/http"
 
-
 	// "github.com/gin-gonic/gin"
 	tb "github.com/090809/telebot"
+	"github.com/joho/godotenv"
 	//"go-kakadu/generated/prisma-client"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		if _, err := os.Stat("./.env.example"); os.IsNotExist(err) {
+			log.Fatalln("File .env.example exists! Rename it to .env, before we go")
+		}
+		log.Fatalf("Error .env loading: %v", err.Error())
+	}
+
 	var (
 		port      = os.Getenv("PORT")
 		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
@@ -31,12 +38,14 @@ func main() {
 		Poller: webhook,
 	}
 
-	b, err := tb.NewBot(pref); if err != nil {
+	b, err := tb.NewBot(pref)
+	if err != nil {
 		log.Fatalln(err)
 	}
 
 	b.Handle("/start", func(m *tb.Message) {
-		_, err := b.Send(m.Sender, "Hi!"); if err != nil {
+		_, err := b.Send(m.Sender, "Hi!")
+		if err != nil {
 			log.Fatalln(err)
 		}
 	})
